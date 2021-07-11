6686e938604a7aa6b7fec305ec631953f0f56c77
Mikasa-z <81673684+Mikasa-z@users.noreply.github.com>
2021-07-11 10:05:12 +0800

    Update 1002. A+B for Polynomials (25).cpp

--- AdvancedLevel_C++/1002. A+B for Polynomials (25).cpp
+++ AdvancedLevel_C++/1002. A+B for Polynomials (25).cpp
@@ -25,3 +25,31 @@ int main() {
     }
     return 0;
 }
+
+// 下面的解法更简洁，但是运行时间可能更长，总体而言就是以数组下标和数组来传递信息的
+
+#include <iostream>
+using namespace std;
+int main(){
+    double a[1010]={0};
+    int m,n,k=0,k1;
+    double k2;
+    cin >> m;
+    for(int i=0;i<m;i++){
+        cin>> k1 >> k2;
+        a[k1]+=k2;
+    }
+    cin >> n;
+    for (int j=0;j<n;j++){
+        cin>> k1 >> k2;
+        a[k1]+=k2;
+    }
+    for (int i=0; i<1001;i++){
+        if(a[i]!=0) k++;
+    }
+    cout << k;
+    for (int i=1000;i>=0;i--){
+        if(a[i]!=0) printf(" %d %.1f",i,a[i]);
+    }
+    return 0;
+}
